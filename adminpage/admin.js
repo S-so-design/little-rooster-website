75a40d391b68bb04a3ff09ca4011afbaada28c76
Rohith Pawar <rohithpawarj@gmail.com>
2026-03-23 13:33:55 -0400

    update the admin and working on adjs

---
+++ adminpage/admin.js
@@ -0,0 +1,21 @@
+// Task 1: Form Handling (Easy Start)
+
+// Goal: Make your form respond when the user submits it.
+
+// Subtasks:
+
+// Detect when the form is submitted.
+// Prevent the default behavior so the page doesn’t reload.
+// Read the value(s) the user entered in the input fields.
+// Display a message to the user, like “Form submitted!” or “Please fill out all fields.”
+// Optionally clear the input fields after submission.
+const productName = document.querySelector("#productName").value
+const brandName = document.querySelector("#brandName").value
+const selectRadio = document.querySelector('input[name = "radioName"]:checked').value
+const saveButton = document.querySelector("#save")
+
+saveButton.addEventListener("click", () => {
+    let arr = []
+    arr.push
+    console.log
+})
